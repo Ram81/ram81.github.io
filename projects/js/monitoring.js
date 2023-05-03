d66ce3764a029a9f35ce06987441fabdd72652b1
Ram81 <ramramrakhya81@gmail.com>
2023-05-02 21:31:05 -0400

    [Projects] Add OVON project page

---
+++ projects/js/monitoring.js
@@ -0,0 +1,128 @@
+var jsonData = [];
+var instructionRecordMap = {};
+var numberPerPage = 50;
+var numberOfPages = 0;
+var currentPage = 1;
+var pageList = new Array();
+var allHitMeta = {};
+
+
+function loadJSON(callback, path) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    //xobj.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && (xobj.status === 200 || xobj.status === 0)) {
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+        }
+    }
+    xobj.send(null);
+
+}
+
+function getNumberOfPages() {
+    var selectedInstruction = document.getElementById("instructionList").value;
+    var selectedScene = document.getElementById("sceneList").value;
+    return Math.ceil(instructionRecordMap[selectedScene][selectedInstruction].length / numberPerPage);
+}
+
+function sampleObservations(records) {
+    var observationsMap = {};
+
+    for (let record in records) {
+        var category = records[record]["object_category"];
+        observationsMap[category] = records[record]["object_goals"];
+    }
+    return observationsMap;
+}
+
+function loadDataset(records) {
+    var count = Object.keys(observationsMap).length;
+    numberOfPages = getNumberOfPages();
+
+    console.log('Total training examples ' + count);
+    console.log('Number of pages: ' + numberOfPages);
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+function getObservationPath(path) {
+    return "https://habitat-on-web.s3.amazonaws.com/ovon/object_goals/" + path;
+}
+
+function drawList(observationsMap) {
+    var page = document.getElementById("main-container");
+    console.log(observationsMap);
+    for (let category in observationsMap) {
+        console.log("Category: " + category);
+        var row = document.createElement("div");
+        row.className = "row";
+
+        var col = document.createElement("div");
+        col.className = "col-md-3";
+        col.innerHTML = "<span> " + category + " </span>"
+        row.appendChild(col);
+
+        for (let idx in observationsMap[category]) {
+            var col = document.createElement("div");
+            col.className = "col-md-3";
+            var obsPath = getObservationPath(observationsMap[category][idx]);
+            var img = getImageElement(obsPath);
+            col.appendChild(img);
+            row.appendChild(col);
+        }
+        page.appendChild(row);
+    }
+}
+
+
+function getImageElement(path) {
+    var img = document.createElement("img");
+    img.src = path;
+    img.width = 300;
+    img.height = 300;
+    return img;
+}
+
+function nextPage() {
+    currentPage += 1;
+    loadList();
+}
+
+function previousPage() {
+    currentPage -= 1;
+    loadList();
+}
+
+function firstPage() {
+    currentPage = 1;
+    loadList();
+}
+
+function lastPage() {
+    currentPage = numberOfPages;
+    loadList();
+}
+
+
+function load() {
+    // populateScenes();
+    loadJSON(function (data) {
+        jsonData = JSON.parse(data);
+        console.log(jsonData)
+        observationsMap = sampleObservations(jsonData);
+        drawList(observationsMap);
+    }, "https://habitat-on-web.s3.amazonaws.com/ovon/object_goals/object_goals.json");
+}
+
+window.onload = load;
